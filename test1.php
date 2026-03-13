5c8b7d4a268987413df63ba965a0dd151edbcb2b
chandrashekar <chandrashekar.com>
2026-03-13 12:57:57 +0530

    updated remote file to install the git hooks project

--- test1.php
+++ test1.php
@@ -1,5 +1,1 @@
-cssaurl -i -u admin:admin123 http://localhost:15672/api
-curl -i -u admin:admindskfj123 http://localhost:15672/api
-curl -i -u admin:admivvfgndskfj123 http://localhost:15672/api
-curl -i -u admsdfdin:adsfdgfmivvfgndskfj123 http://localhost:15672/api
-curl -i -u admsdfdin:adsfdgfmivvfgndskfj123 http://localhost:15672/api
+dsfsgfffd
