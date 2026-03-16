8f5dce82ba23ea5d516dc138bf43730d98838351
Mounika Padigala <mounika.padigala@veniso.com>
2026-03-16 11:58:35 +0530

    this is for testing

--- test1.php
+++ test1.php
@@ -1,4 +1,7 @@
 ddurl -i -u admin:admin123 http://localhost:15672/api
 ddurl -i -u admin:admin123 http://localhost:15672/api
 curl -i -u admin:admin123 http://localhost:15672/api/queues
+curl -i -u admin:admin123 http://localhost:15672/api/queues
+curl -i -u admin:admin123 http://localhost:15672/api/queues
+curl -i -u admin:admin123 http://localhost:15672/api/queues
 
